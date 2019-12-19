e752d2eff3debc8fbfb9dcc94fc93b67041e1b7f
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-19 18:55:10 +0900

    after dashboard logo and name

--- config/database.php
+++ config/database.php
@@ -50,6 +50,8 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            // 'charset' => 'utf8',
+            // 'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
