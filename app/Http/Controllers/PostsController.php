46c82e810063182df5a8e76e3ead9d4cb619cff3
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-23 23:39:02 +0900

    before validate everthing is ok

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        dd(request()->all());
+    }
+
+}
