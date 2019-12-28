4069a411b8f96d375c7d4c1f038736b2bd500778
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-28 18:53:38 +0900

    after profile edit fully done

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -14,11 +15,15 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
+
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user)
     {
+        $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -26,7 +31,20 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        auth()->user()->profile->update($data);
+
+
+        if (request('image'))
+        {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("/storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save(); 
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
 
         return redirect("/profile/{$user->id}");
     }
